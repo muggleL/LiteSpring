87ab678b34881034dade6893237f0c18ce1dbfe2
muggleL <ffshine@hotmail.com>
2019-11-23 15:54:29 +0800

    添加测试用例

--- src/test/java/de/o0o0o0/v2/test/BeanDefinitionValueResolverTest.java
+++ src/test/java/de/o0o0o0/v2/test/BeanDefinitionValueResolverTest.java
@@ -5,6 +5,7 @@ import de.o0o0o0.v7.beans.factory.support.BeanDefinitionValueResolver;
 import de.o0o0o0.v7.beans.factory.support.DefaultBeanFactory;
 import de.o0o0o0.v7.beans.factory.xml.XmlBeanDefinitionReader;
 import de.o0o0o0.v7.config.RuntimeBeanReference;
+import de.o0o0o0.v7.config.TypedStringValue;
 import de.o0o0o0.v7.core.io.ClassPathResource;
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,5 +23,18 @@ public class BeanDefinitionValueResolverTest {
 
         Assert.assertNotNull(value);
         Assert.assertTrue(value instanceof AccountDao);
+    }
+    @Test
+    public void testResolveTypedStringValue() {
+        DefaultBeanFactory factory = new DefaultBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.LoadBeanDefinitionReader(new ClassPathResource("petstore-v2.xml"));
+
+        BeanDefinitionValueResolver resolver = new BeanDefinitionValueResolver(factory);
+
+        TypedStringValue value = new TypedStringValue("test");
+        Object val = resolver.resolveValueIfNecessary(value);
+        Assert.assertNotNull(val);
+        Assert.assertEquals(val, "test");
     }
 }
