48f8d061dbe324e49bff75077fa0977e0e88e89f
muggleL <ffshine@hotmail.com>
2019-11-23 15:48:59 +0800

    添加类BeanDefinitionValueResolverTest 获取 String类型的 value

--- src/main/java/de/o0o0o0/v7/beans/factory/support/BeanDefinitionValueResolver.java
+++ src/main/java/de/o0o0o0/v7/beans/factory/support/BeanDefinitionValueResolver.java
@@ -1,6 +1,7 @@
 package de.o0o0o0.v7.beans.factory.support;
 
 import de.o0o0o0.v7.config.RuntimeBeanReference;
+import de.o0o0o0.v7.config.TypedStringValue;
 
 public class BeanDefinitionValueResolver {
     private final DefaultBeanFactory factory;
@@ -13,6 +14,8 @@ public class BeanDefinitionValueResolver {
         if (value instanceof RuntimeBeanReference) {
             String refName = ((RuntimeBeanReference) value).getBeanName();
             return factory.getBean(refName);
+        } else if (value instanceof TypedStringValue) {
+            return ((TypedStringValue) value).getValue();
         }
         throw new RuntimeException("the value " + value + " has not implemented");
     }
